c7af67f97cb671dfda2678c3d2d5fe82b4813a5d
Jack Gould <johnhilsmangould@gmail.com>
2020-07-15 16:34:40 -0600

    add highest revenue seach

---
+++ app/controllers/api/v1/merchants_bi_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::MerchantsBiController < ApplicationController
+  def index
+    quantity = params[:quantity].to_i
+    # merchants = Merchant.joins(invoices: [:invoice_items, :transactions]).where("invoices.status='shipped' and transactions.result='success'").select("merchants.name,  sum(invoice_items.quantity * invoice_items.unit_price) as revenue").group("merchants.name").order("revenue desc").limit(quantity)
+    merchants = Merchant.joins(invoices: [:invoice_items, :transactions]).where("invoices.status='shipped' and transactions.result='success'").select("merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) as revenue").group("merchants.id").order("revenue desc").limit(quantity)
+    render json: MerchantSerializer.new(merchants)
+
+  end
+
+  # def show
+  #   quantity = params[:quantity].to_i
+  #   merchants = Merchant.joins(invoices: [:invoice_items, :transactions]).where("invoices.status='shipped' and transactions.result='success'").group("merchants.name").select("merchants.name, sum(invoice_items.quantity) as items_sold").order("items_sold desc").limit(8)
+  #   render json: MerchantSerializer.new(merchants)
+  # end
+
+end
